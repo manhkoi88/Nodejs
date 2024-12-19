88ce38b74fca2fac460c540a3f61fab926d70231
Manh koi <netkimkhe@gmail.com>
2024-12-19 15:03:51 +0700

    Nodejs demo

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser"; //query /user?id=7
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from './route/web';
+require('dotenv').config();
+
+let app = express();
+//config app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+viewEngine(app);
+initWebRoutes(app);
+
+let port = process.env.PORT || 6969;
+//port === undefined => port = 6969
+app.listen(port, () => {
+    //callback
+    console.log("Backend Nodejs is running on the port : " + port)
+})
